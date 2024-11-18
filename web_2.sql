1f65ddabb93e68d24e19b57655e69c6bf8e5db2a
Luan314ds <133255561+Luan314ds@users.noreply.github.com>
2024-11-18 00:14:05 -0300

    Update and rename tp__1_web_2.sql to web_2.sql
    modificamos la base de datos

--- web_2.sql
+++ web_2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2024 a las 23:44:30
+-- Tiempo de generación: 18-11-2024 a las 04:12:08
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `tp°1_web_2`
+-- Base de datos: `web 2`
 --
 
 -- --------------------------------------------------------
@@ -39,9 +39,8 @@ CREATE TABLE `marca` (
 --
 
 INSERT INTO `marca` (`id`, `nombre_marca`, `importador`, `pais_origen`) VALUES
-(1, 'Samsung', 'Italia', 'Estados Unidos'),
-(2, 'Lenovo', 'Belgica', 'Brasil'),
-(3, 'Apple', 'Rusia', 'China');
+(7, 'MOTOROLA', 'INDIA', 'SURINAM'),
+(12, 'APPLE', 'REPUBLICA CHECA', 'BRASIL');
 
 -- --------------------------------------------------------
 
@@ -51,23 +50,41 @@ INSERT INTO `marca` (`id`, `nombre_marca`, `importador`, `pais_origen`) VALUES
 
 CREATE TABLE `productos` (
   `id_productos` int(11) NOT NULL,
-  `id_marca` int(11) NOT NULL,
+  `marca_producto` varchar(100) NOT NULL,
   `tipo_producto` varchar(100) NOT NULL,
   `modelo` varchar(100) NOT NULL,
-  `color` varchar(100) NOT NULL
+  `color` varchar(100) NOT NULL,
+  `descripcion_producto` text NOT NULL,
+  `precio` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `productos`
 --
 
-INSERT INTO `productos` (`id_productos`, `id_marca`, `tipo_producto`, `modelo`, `color`) VALUES
-(1, 3, 'Celular ', 'iPhone 16 Pro Max', 'Violeta'),
-(2, 1, 'Heladera', 'SpaceMax 409L', 'Negro'),
-(3, 2, 'Notebook', 'IdeaPad 15', 'Azul marino'),
-(4, 3, 'HeadPhones', 'AirPods', 'Blanco'),
-(5, 1, 'Televisor', 'SNK28-2024', 'Negro'),
-(6, 2, 'Tablet', 'M11 11', 'Blanca');
+INSERT INTO `productos` (`id_productos`, `marca_producto`, `tipo_producto`, `modelo`, `color`, `descripcion_producto`, `precio`) VALUES
+(15, 'MOTOROLA', 'CELULAR', 'G24 power', 'rosa', 'NUEVO', 250000),
+(16, 'APPLE', 'MacBoock', '15 plus', 'negra', 'roto', 200000000),
+(21, 'MOTOROLA', 'CELULAR', 'G30', 'AZUL', 'NUEVO', 30);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_login` int(11) NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `contraseña` char(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_login`, `nombre`, `contraseña`) VALUES
+(1, 'webadmin', '$2y$10$V.sveVn3H8cqyCubIptTGuN1vrGR6iKoGgwjmSjhCfvH9iYsT3QFu');
 
 --
 -- Índices para tablas volcadas
@@ -77,14 +94,22 @@ INSERT INTO `productos` (`id_productos`, `id_marca`, `tipo_producto`, `modelo`, 
 -- Indices de la tabla `marca`
 --
 ALTER TABLE `marca`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQUE` (`nombre_marca`);
 
 --
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id_productos`),
-  ADD KEY `marca_id_marca_productos` (`id_marca`);
+  ADD KEY `marca_marca_producto_productos` (`marca_producto`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_login`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -94,13 +119,19 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `marca`
 --
 ALTER TABLE `marca`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id_productos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_productos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_login` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
@@ -110,9 +141,10 @@ ALTER TABLE `productos`
 -- Filtros para la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD CONSTRAINT `marca_id_marca_productos` FOREIGN KEY (`id_marca`) REFERENCES `marca` (`id`);
+  ADD CONSTRAINT `marca_marca_producto_productos` FOREIGN KEY (`marca_producto`) REFERENCES `marca` (`nombre_marca`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
